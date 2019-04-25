dd0d8e68fcdf0cc9742040bb6abba44c64953be9
Riley Hilder <rileyhilder@Rileys-MacBook-Pro.local>
2019-04-24 21:08:14 -0400

    DB Config

--- server/dbconfig.js
+++ server/dbconfig.js
@@ -1,10 +1,10 @@
 const getDbConfig = () => {
     let dbConfig = {
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST || 'ec2-54-243-197-120.compute-1.amazonaws.com',
       port: process.env.DB_PORT || 5432,
-      database: process.env.DB_DATABASE || 'sevenheaven_local',
-      user: process.env.DB_USER || '',
-      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || 'd4v1chttm3uh13',
+      user: process.env.DB_USER || 'mxydluztjcvapm',
+      password: process.env.DB_PASSWORD || 'c6629f27ae3a15fbab688dfe97898cea9f4a7fd922aa8c30c6a649fb2e58f9d3',
       poolSize: 3,
     };
     
